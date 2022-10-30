f72ed6f6e9789e9a8ecb91406af2c5da844e6aaf
= <guy.r@fingen.co.il>
2022-10-30 14:11:14 +0200

    path fix

--- collect4easyplot/data_grabber.py
+++ collect4easyplot/data_grabber.py
@@ -7,7 +7,7 @@ TMP_PATH = os.environ.get("TEMP")
 
 def collect(data: Any, name: str):
     """ Dump data to file object in TEMP dir. Supposed to be used by EasyPlot """
-    file_path = os.path.join(TMP_PATH, f"{name}.obj")
+    file_path = os.path.join(TMP_PATH, "EasyPlotDataGrabber", f"{name}.obj")
     file_obj = open(file_path, 'wb')
     pickle.dump(data, file_obj)
     file_obj.close()
